db8b0886c65ccc257360aba42a47d0d75800428e
Guillermo Jofre <jofregf@gmail.com>
2022-10-11 12:22:01 -0300

    sesiones 7, 8 y 9 - Ejercicio 3

--- src/test/java/com/example/obspringsesion456/controllers/LaptopControllerTest.java
+++ src/test/java/com/example/obspringsesion456/controllers/LaptopControllerTest.java
@@ -25,6 +25,46 @@ class LaptopControllerTest {
 
     @LocalServerPort
     private int port;
+
+    public void cargarLaptops(){
+        // Creamos laptop y definimos formato
+        HttpHeaders headers = new HttpHeaders();
+        // Formato JSON
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        String laptop1 = """
+            {
+            
+            "marca": "Lenovo",
+            "modelo": "Phantom Black",
+            "procesador": "Intel Core i5",
+            "pantalla": 15.6,
+            "almacenamiento": 128,
+            "ram": 8,
+            "precio": 230999.0,
+            "windows": true
+            }
+                """;
+        String laptop2 = """
+            {
+            
+            "marca": "Acer", 
+            "modelo": "A315-34", 
+            "procesador": "Intel Celeron", 
+            "pantalla": 15.6, 
+            "almacenamiento": 500, 
+            "ram": 4, "precio": 99000.00, 
+            "windows": true
+            }
+        """;
+        // Definimos los datos a pasar
+        HttpEntity<String> request1 = new HttpEntity<>(laptop1, headers);
+        HttpEntity<String> request2 = new HttpEntity<>(laptop2, headers);
+
+        // Pasamos los datos
+        ResponseEntity<Laptop> response1 = testRestTemplate.exchange("/api/laptops", HttpMethod.POST, request1, Laptop.class);
+        ResponseEntity<Laptop> response2 = testRestTemplate.exchange("/api/laptops", HttpMethod.POST, request2, Laptop.class);
+    }
     @BeforeEach
     void setUp() {
         restTemplateBuilder = restTemplateBuilder.rootUri("http://localhost:" + port);
@@ -33,9 +73,29 @@ class LaptopControllerTest {
     @DisplayName("Comprueba que se busque todas las laptops desde los controladores")
     @Test
     void findAll() {
+        cargarLaptops();
         ResponseEntity<Laptop[]> response = testRestTemplate.getForEntity("/api/laptops", Laptop[].class);
-        assertEquals(HttpStatus.OK, response.getStatusCode());
         List<Laptop> laptop = Arrays.asList(response.getBody());
+        if(response.getBody() != null){
+            assertEquals(HttpStatus.OK, response.getStatusCode());
+//            assertEquals(2, laptop.size());
+            assertTrue(laptop.size()>1);
+        } else {
+            assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        }
+
+    }
+
+    @DisplayName("Comprueba buscar una laptop con id")
+    @Test
+    void findOneById() {
+        cargarLaptops();
+        ResponseEntity<Laptop> response = testRestTemplate.getForEntity("/api/laptops/2", Laptop.class);
+        Laptop result = response.getBody();
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assert result != null;
+        assertEquals("Lenovo", result.getMarca());
+        assertEquals("Phantom Black", result.getModelo());
     }
 
     @DisplayName("Comprueba crear una laptop desde controladores")
@@ -64,22 +124,50 @@ class LaptopControllerTest {
         assertEquals(256, result.getAlmacenamiento());
 
     }
-    @DisplayName("Comprueba buscar una laptop con id")
-    @Test
-    void findOneById() {
-        ResponseEntity<Laptop> response = testRestTemplate.getForEntity("/api/laptops/1", Laptop.class);
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-    }
-
     @Test
     void update() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        String actualizar = """
+                {
+                "id": 3,
+                "marca": "Acer Test",
+                "modelo": "A315-34 Test",
+                "procesador": "Intel Celeron",
+                "pantalla": 15.6,
+                "almacenamiento": 500,
+                "ram": 4, "precio": 99000.00,
+                "windows": true
+                }
+                """;
+        HttpEntity<String> request = new HttpEntity<>(actualizar, headers);
+        ResponseEntity<Laptop> response = testRestTemplate.exchange("/api/laptops", HttpMethod.PUT, request, Laptop.class);
+        Laptop result = response.getBody();
+        if (!response.getStatusCode().isError()){
+            assertEquals("Acer Test", result.getMarca());
+            assertEquals("A315-34 Test", result.getModelo());
+        }
     }
 
     @Test
     void delete() {
+        cargarLaptops();
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> request = new HttpEntity<>(headers);
+        ResponseEntity<Laptop> response = testRestTemplate.exchange("/api/laptops/1", HttpMethod.DELETE, request, Laptop.class);
+        assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
     }
 
     @Test
     void deleteAll() {
+        cargarLaptops();
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> request = new HttpEntity<>(headers);
+        ResponseEntity<Laptop> response = testRestTemplate.exchange("/api/laptops", HttpMethod.DELETE, request, Laptop.class);
+        assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
+        ResponseEntity<Laptop[]> response2 = testRestTemplate.getForEntity("/api/laptops", Laptop[].class);
+        List<Laptop> laptops = Arrays.asList(response2.getBody());
+        assertFalse(laptops.size() != 0);
     }
 }
